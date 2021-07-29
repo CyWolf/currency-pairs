4c775c6545014b87a02cff4a30f8b8208b59b2f4
Luis Delgado <72816266+CyWolf@users.noreply.github.com>
2021-07-28 22:33:27 -0400

    Initial commit

---
+++ src/modules/view/component/view.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function View() {
+  return (
+    <div className='container mt-5'>
+      <div className='text-center'>
+        <h3>Project One</h3>
+      </div>
+      <div className='text-center mt-4'>
+        <p>Project One</p>
+      </div>
+    </div>
+  )
+}
+
+export default View
---
+++ src/modules/view/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import View from '../component/view'
+
+function MainView() {
+  return (
+    <div>
+      <View />
+    </div>
+  )
+}
+
+export default MainView
